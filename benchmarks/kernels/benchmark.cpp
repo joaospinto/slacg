adb2e78391c9535cf4a64acaf8a8160022cefb96
João Sousa-Pinto <joaospinto@gmail.com>
2026-06-12 15:12:25 -0700

    Report benchmark medians

--- benchmarks/kernels/benchmark.cpp
+++ benchmarks/kernels/benchmark.cpp
@@ -9,8 +9,19 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <vector>
 
 namespace {
+
+volatile double benchmark_sink = 0.0;
+
+template <std::size_t N> double checksum(const std::array<double, N> &data) {
+  double result = 0.0;
+  for (double value : data) {
+    result += value;
+  }
+  return result;
+}
 
 template <typename Fn>
 double benchmark_ns_per_call(const int iterations, Fn fn) {
@@ -25,6 +36,18 @@ double benchmark_ns_per_call(const int iterations, Fn fn) {
   return static_cast<double>(total_ns) / static_cast<double>(iterations);
 }
 
+template <typename Fn, typename ChecksumFn>
+std::vector<double> benchmark_samples(const int iterations, const int samples,
+                                      Fn fn, ChecksumFn checksum_fn) {
+  std::vector<double> results;
+  results.reserve(samples);
+  for (int i = 0; i < samples; ++i) {
+    results.push_back(benchmark_ns_per_call(iterations, fn));
+    benchmark_sink += checksum_fn();
+  }
+  return results;
+}
+
 int iterations_from_args(const int argc, char **argv) {
   if (argc < 2) {
     return 10000;
@@ -32,33 +55,45 @@ int iterations_from_args(const int argc, char **argv) {
   return std::max(1, std::atoi(argv[1]));
 }
 
-template <std::size_t N>
-void fill_data(std::array<double, N> &data) {
+int samples_from_args(const int argc, char **argv) {
+  if (argc < 3) {
+    return 5;
+  }
+  return std::max(1, std::atoi(argv[2]));
+}
+
+template <std::size_t N> void fill_data(std::array<double, N> &data) {
   for (std::size_t i = 0; i < data.size(); ++i) {
     data[i] = 1.0 + static_cast<double>(i % 7) * 0.01;
   }
 }
 
 void print_result(const std::string &name, const int iterations,
-                  const double ns_per_call) {
-  std::cout << name << ": " << ns_per_call << " ns/call"
-            << " (" << iterations << " iterations)\n";
+                  std::vector<double> samples) {
+  std::sort(samples.begin(), samples.end());
+  const double median = samples[samples.size() / 2];
+  std::cout << name << ": " << median << " ns/call median"
+            << " (min " << samples.front() << ", max " << samples.back() << ", "
+            << iterations << " iterations, " << samples.size() << " samples)\n";
 }
 
-void run_ldlt_factor(const int iterations) {
+void run_ldlt_factor(const int iterations, const int samples) {
   std::array<double, 170> A_data{};
   std::array<double, slacg::bench::ldlt::L_nnz> LT_data{};
   std::array<double, slacg::bench::ldlt::dim> D_inv{};
   fill_data(A_data);
 
   print_result("ldlt_factor", iterations,
-               benchmark_ns_per_call(iterations, [&] {
-                 slacg::bench::ldlt::ldlt_factor(A_data.data(), LT_data.data(),
-                                                  D_inv.data());
-               }));
+               benchmark_samples(
+                   iterations, samples,
+                   [&] {
+                     slacg::bench::ldlt::ldlt_factor(
+                         A_data.data(), LT_data.data(), D_inv.data());
+                   },
+                   [&] { return checksum(LT_data) + checksum(D_inv); }));
 }
 
-void run_ldlt_solve(const int iterations) {
+void run_ldlt_solve(const int iterations, const int samples) {
   std::array<double, 170> A_data{};
   std::array<double, slacg::bench::ldlt::L_nnz> LT_data{};
   std::array<double, slacg::bench::ldlt::dim> D_inv{};
@@ -66,17 +101,19 @@ void run_ldlt_solve(const int iterations) {
   std::array<double, slacg::bench::ldlt::dim> x{};
   fill_data(A_data);
   fill_data(b);
-  slacg::bench::ldlt::ldlt_factor(A_data.data(), LT_data.data(),
-                                  D_inv.data());
+  slacg::bench::ldlt::ldlt_factor(A_data.data(), LT_data.data(), D_inv.data());
 
   print_result("ldlt_solve", iterations,
-               benchmark_ns_per_call(iterations, [&] {
-                 slacg::bench::ldlt::ldlt_solve(LT_data.data(), D_inv.data(),
-                                                b.data(), x.data());
-               }));
+               benchmark_samples(
+                   iterations, samples,
+                   [&] {
+                     slacg::bench::ldlt::ldlt_solve(
+                         LT_data.data(), D_inv.data(), b.data(), x.data());
+                   },
+                   [&] { return checksum(x); }));
 }
 
-void run_matvec(const int iterations) {
+void run_matvec(const int iterations, const int samples) {
   std::array<double, 240> A_data{};
   std::array<double, 48> x{};
   std::array<double, 72> y{};
@@ -84,14 +121,17 @@ void run_matvec(const int iterations) {
   fill_data(x);
 
   print_result("matvec_add_Ax", iterations,
-               benchmark_ns_per_call(iterations, [&] {
-                 y.fill(0.0);
-                 slacg::bench::matvec::add_Ax_to_y(A_data.data(), x.data(),
-                                                   y.data());
-               }));
+               benchmark_samples(
+                   iterations, samples,
+                   [&] {
+                     y.fill(0.0);
+                     slacg::bench::matvec::add_Ax_to_y(A_data.data(), x.data(),
+                                                       y.data());
+                   },
+                   [&] { return checksum(y); }));
 }
 
-void run_gtwg(const int iterations) {
+void run_gtwg(const int iterations, const int samples) {
   std::array<double, 192> G_data{};
   std::array<double, 64> w{};
   std::array<double, 105> gt_w_g{};
@@ -99,13 +139,16 @@ void run_gtwg(const int iterations) {
   fill_data(w);
 
   print_result("gt_w_g", iterations,
-               benchmark_ns_per_call(iterations, [&] {
-                 slacg::bench::gtwg::gt_w_g(G_data.data(), w.data(), 1e-3,
-                                            gt_w_g.data());
-               }));
+               benchmark_samples(
+                   iterations, samples,
+                   [&] {
+                     slacg::bench::gtwg::gt_w_g(G_data.data(), w.data(), 1e-3,
+                                                gt_w_g.data());
+                   },
+                   [&] { return checksum(gt_w_g); }));
 }
 
-void run_kkt_factor(const int iterations) {
+void run_kkt_factor(const int iterations, const int samples) {
   std::array<double, 90> H_data{};
   std::array<double, 96> C_data{};
   std::array<double, 120> G_data{};
@@ -122,15 +165,18 @@ void run_kkt_factor(const int iterations) {
   r3.fill(1e-3);
 
   print_result("kkt_ldlt_factor", iterations,
-               benchmark_ns_per_call(iterations, [&] {
-                 static_cast<void>(slacg::bench::kkt::ldlt_factor(
-                     H_data.data(), C_data.data(), G_data.data(), w.data(),
-                     1e-3, r2.data(), r3.data(), LT_data.data(),
-                     D_inv.data()));
-               }));
+               benchmark_samples(
+                   iterations, samples,
+                   [&] {
+                     static_cast<void>(slacg::bench::kkt::ldlt_factor(
+                         H_data.data(), C_data.data(), G_data.data(), w.data(),
+                         1e-3, r2.data(), r3.data(), LT_data.data(),
+                         D_inv.data()));
+                   },
+                   [&] { return checksum(LT_data) + checksum(D_inv); }));
 }
 
-void run_kkt_solve(const int iterations) {
+void run_kkt_solve(const int iterations, const int samples) {
   std::array<double, 90> H_data{};
   std::array<double, 96> C_data{};
   std::array<double, 120> G_data{};
@@ -153,13 +199,16 @@ void run_kkt_solve(const int iterations) {
       r3.data(), LT_data.data(), D_inv.data()));
 
   print_result("kkt_ldlt_solve", iterations,
-               benchmark_ns_per_call(iterations, [&] {
-                 slacg::bench::kkt::ldlt_solve(LT_data.data(), D_inv.data(),
-                                               b.data(), x.data());
-               }));
+               benchmark_samples(
+                   iterations, samples,
+                   [&] {
+                     slacg::bench::kkt::ldlt_solve(LT_data.data(), D_inv.data(),
+                                                   b.data(), x.data());
+                   },
+                   [&] { return checksum(x); }));
 }
 
-void run_kkt_add_Kx(const int iterations) {
+void run_kkt_add_Kx(const int iterations, const int samples) {
   std::array<double, 90> H_data{};
   std::array<double, 96> C_data{};
   std::array<double, 120> G_data{};
@@ -182,28 +231,34 @@ void run_kkt_add_Kx(const int iterations) {
   r2.fill(1e-3);
   r3.fill(1e-3);
 
-  print_result("kkt_add_Kx_to_y", iterations,
-               benchmark_ns_per_call(iterations, [&] {
-                 y_x.fill(0.0);
-                 y_y.fill(0.0);
-                 y_z.fill(0.0);
-                 slacg::bench::kkt::add_Kx_to_y(
-                     H_data.data(), C_data.data(), G_data.data(), w.data(),
-                     1e-3, r2.data(), r3.data(), x_x.data(), x_y.data(),
-                     x_z.data(), y_x.data(), y_y.data(), y_z.data());
-               }));
+  print_result(
+      "kkt_add_Kx_to_y", iterations,
+      benchmark_samples(
+          iterations, samples,
+          [&] {
+            y_x.fill(0.0);
+            y_y.fill(0.0);
+            y_z.fill(0.0);
+            slacg::bench::kkt::add_Kx_to_y(
+                H_data.data(), C_data.data(), G_data.data(), w.data(), 1e-3,
+                r2.data(), r3.data(), x_x.data(), x_y.data(), x_z.data(),
+                y_x.data(), y_y.data(), y_z.data());
+          },
+          [&] { return checksum(y_x) + checksum(y_y) + checksum(y_z); }));
 }
 
-}  // namespace
+} // namespace
 
 int main(int argc, char **argv) {
   const int iterations = iterations_from_args(argc, argv);
-  run_ldlt_factor(iterations);
-  run_ldlt_solve(iterations);
-  run_matvec(iterations);
-  run_gtwg(iterations);
-  run_kkt_factor(iterations);
-  run_kkt_solve(iterations);
-  run_kkt_add_Kx(iterations);
+  const int samples = samples_from_args(argc, argv);
+  run_ldlt_factor(iterations, samples);
+  run_ldlt_solve(iterations, samples);
+  run_matvec(iterations, samples);
+  run_gtwg(iterations, samples);
+  run_kkt_factor(iterations, samples);
+  run_kkt_solve(iterations, samples);
+  run_kkt_add_Kx(iterations, samples);
+  std::cout << "checksum: " << benchmark_sink << "\n";
   return 0;
 }
